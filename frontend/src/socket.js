a5748769401a076ef0731cd4d95e34fda7d604e4
Vimal Kumar <vimalgodara514@gmail.com>
2026-06-24 17:54:35 +0530

    Fix production API URL

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://intellmeet-ai-powered-enterprise-meeting-nyy6.onrender.com", {
   autoConnect: true,
 });
 
